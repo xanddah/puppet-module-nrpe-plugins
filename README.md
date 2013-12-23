87c6bc664a4dae58f1f3ea70e9d4f9462026be2c
anders-k-lindgren <anders.k.lindgren@ericsson.com>
2013-12-23 17:32:22 +0100

    initial release

--- README.md
+++ README.md
@@ -1,4 +1,66 @@
-puppet-module-nrpe-plugins
-==========================
+# puppet-module-nrpe-plugins
+===
+
+[![Build Status](https://travis-ci.org/anders-k-lindgren/puppet-module-nrpe-plugins.png?branch=master)](https://travis-ci.org/anders-k-lindgren/puppet-module-nrpe-plugins)
 
 Distribute nrpe plugins
+
+===
+
+# Compatibility
+---------------
+This module is built for use with Puppet v3 and is tested on the following OS families.
+
+* EL 6
+* Suse 11
+* Solaris 10
+
+===
+
+# Class `nrpe_plugins`
+
+## Parameters
+
+nrpe_package
+------------
+Name of package(s) for NRPE.
+
+- *Default*: based on OS platform.
+
+source
+-----------
+Directory in which the nrpe plugins are read.
+
+
+- *Required*: 
+
+destination
+-----------
+Directory in which nrpe plugins are stored.
+
+- *Required*: 
+
+nrpe_user
+---------
+This determines the effective user that the NRPE daemon should run as.
+
+- *Default*: based on OS platform
+
+nrpe_group
+---------
+This determines the effective group that the NRPE daemon should run as.
+
+- *Default*: based on OS platform
+
+force
+-----
+Boolean value to deside if the file operation should performed even if it will destroy one or more directories.
+
+- *Default*: true
+
+purge
+-----
+Boolean value to deside if unmanaged files should be purged. 
+
+- *Default*: true
+
---
+++ manifests/init.pp
@@ -0,0 +1,91 @@
+# == Class: nrpe_plugins
+#
+# This module distribute all nrpe plugins from a directory
+#
+# === Parameters
+#
+# All parameters are described in the README file
+#
+
+class nrpe_plugins (
+  $nrpe_plugins_ensure = 'present',
+  $nrpe_package        = 'USE_DEFAULTS',
+  $nrpe_user           = 'USE_DEFAULTS',
+  $nrpe_group          = 'USE_DEFAULTS',
+  $source              = undef,
+  $destination         = undef,
+  $purge               = true,
+  $force               = true,
+) {
+
+  # OS platform defaults
+  case $::osfamily {
+    'RedHat': {
+      $default_nrpe_package = 'nrpe'
+      $default_nrpe_user    = 'nrpe'
+      $default_nrpe_group   = 'nrpe'
+    }
+    'Suse': {
+      $default_nrpe_package = 'nagios-nrpe'
+      $default_nrpe_user    = 'nagios'
+      $default_nrpe_group   = 'nagios'
+
+    }
+    'Solaris': {
+      $default_nrpe_package = undef
+      $default_nrpe_user    = 'nagios'
+      $default_nrpe_group   = 'nagios'
+    }
+    default: {
+      fail("nrpe_plugins supports osfamilies RedHat, Suse and Solaris. Detected osfamily is <${::osfamily}>.")
+    }
+  }
+
+  # Use defaults if a value was not specified in Hiera.
+  if $nrpe_package == 'USE_DEFAULTS' {
+    $nrpe_package_real = $default_nrpe_package
+  } else {
+    $nrpe_package_real = $nrpe_package
+  }
+
+  if $nrpe_user == 'USE_DEFAULTS' {
+    $nrpe_user_real = $default_nrpe_user
+  } else {
+    $nrpe_user_real = $nrpe_user
+  }
+
+  if $nrpe_group == 'USE_DEFAULTS' {
+    $nrpe_group_real = $default_nrpe_group
+  } else {
+    $nrpe_group_real = $nrpe_group
+  }
+
+  #--- Check parameters ---
+
+  if $source != '' and $destination == '' {
+    fail( 'source is set, destination is mandatory' )
+  }
+
+  # Validates $plugins_contrib_ensure
+  case $nrpe_plugins_ensure {
+    'file', 'present', 'absent': {
+      # noop, these values are valid
+    }
+    default: {
+      fail("Valid values for \$nrpe_plugins_ensure are \'absent\', \'file\', or \'present\'. Specified value is ${nrpe_plugins_ensure}")
+    }
+  }
+
+  validate_absolute_path($destination)
+
+  file { $destination :
+    ensure  => $nrpe_plugins_ensure,
+    source  => $source,
+    recurse => true,
+    purge   => $purge,
+    force   => $force,
+    owner   => $nrpe_user_real,
+    group   => $nrpe_group_real,
+    require => Package[$nrpe_package_real],
+  }
+}
